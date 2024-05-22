157d5c8b9a49534d8508ff533a6aba381c80ecac
JosueEC <josueev.cruz@gmail.com>
2024-05-21 19:12:18 -0600

    [FEAT](utility): Add the password utility to manage the password encrypt

---
+++ app/utility/password.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt';
+
+export const getSalt = async () => await bcrypt.genSalt();
+
+export const getHashedPassword = async (password: string, salt: string) => {
+    return await bcrypt.hash(password, salt);
+};
+
+export const validatePassword = async (
+    enteredPassword: string,
+    savedPassword: string,
+    salt: string,
+) => {
+    return (await getHashedPassword(enteredPassword, salt)) === savedPassword;
+};
