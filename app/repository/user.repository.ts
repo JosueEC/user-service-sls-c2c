47cc9a5353af94b92bcad507589562da28ed03ed
JosueEC <josueev.cruz@gmail.com>
2024-05-21 19:13:53 -0600

    [FEAT](user-service): Complete the createUser function in the user.service and user.repository

--- app/repository/user.repository.ts
+++ app/repository/user.repository.ts
@@ -1,7 +1,33 @@
+import { DBCLient } from "../config/database-client";
+import { UserModel } from "../models/user.model";
+
 export class UserRepository {
     constructor() {}
 
-    async createUserOperation() {
-        console.log('User created in DB');
+    async createAccount ({
+        phone,
+        email,
+        password,
+        salt,
+        userType
+    }: UserModel) {
+        /**
+         * Esta es una forma básica en la que podemos crear una conexión
+         * con una base de datos PostgreSQL y ejecutar una query sobre
+         * la misma
+         */
+        const client = await DBCLient();
+        await client.connect();
+
+        const queryString = 'INSERT INTO users(phone,email,password,salt,user_type) VALUES($1,$2,$3,$4,$5) RETURNING *';
+        const values = [phone, email, password, salt, userType];
+
+        const result = await client.query(queryString, values);
+        await client.end();
+
+        if (result.rowCount > 0) {
+            console.log('result->', result.rows[0]);
+            return result.rows[0] as UserModel;
+        }
     }
 }
--- app/service/user.service.ts
+++ app/service/user.service.ts
@@ -1,7 +1,12 @@
+import { ErrorResponse, SuccessResponse } from "../utility/response";
 import { UserRepository } from "../repository/user.repository";
-import { SuccessResponse } from "../utility/response";
+import { SignupInput } from "../models/dto/signup-input.dto";
+import { AppValidationError } from "../utility/errors";
 import { APIGatewayProxyEventV2 } from "aws-lambda";
+import { plainToClass } from "class-transformer";
 import { autoInjectable } from "tsyringe";
+import { UserType } from "../models/enums/user-type.enum";
+import { getHashedPassword, getSalt } from "../utility/password";
 
 /**
  * Para poder inyectar la dependencia en el container es necesario
@@ -17,18 +22,43 @@ export class UserService {
     }
 
     async createUser(event: APIGatewayProxyEventV2) {
-        /**
-         * Dado que usamos middy en el handler, con el middleware de
-         * jsonBodyParser, este middleware ya parsea la solicitud, por
-         * lo que no es necesario usar el JSON.parse, ya que el body
-         * ahora vendra parseado por defecto
-         */
-        const body = event.body;
-        console.log(body);
+        try {
+            /**
+             * Dado que usamos middy en el handler, con el middleware de
+             * jsonBodyParser, este middleware ya parsea la solicitud, por
+             * lo que no es necesario usar el JSON.parse, ya que el body
+             * ahora vendrá parseado por defecto
+             */
+            // const body = event.body;
+            /**
+             * plainToClass convierte texto plano(objeto) a una clase la
+             * cual es pasada como parámetro
+             */
+            const signupInput = plainToClass(SignupInput, event.body);
 
-        await this.userRepository.createUserOperation();
+            /**
+             * Esta es la forma en la que ejecutamos las validaciones
+             * de class-validator y class-transformer, esto ocurre
+             * con lo que definimos en nuestra función AppValidationError
+             */
+            const error = await AppValidationError(signupInput);
+            if (error) return ErrorResponse({code: 404, error });
 
-        return SuccessResponse({ message: 'response from createUser' });
+            const salt = await getSalt();
+            const hashedPassword = await getHashedPassword(signupInput.password, salt);
+            const data = await this.userRepository.createAccount({
+                email: signupInput.email,
+                password: hashedPassword,
+                phone: signupInput.phone,
+                userType: UserType.BUYER,
+                salt: salt,
+            });
+
+            return SuccessResponse(data);
+        } catch (error) {
+            console.error(error);
+            return ErrorResponse({ code: 500, error });
+        }
     }
 
     async loginUser(event: APIGatewayProxyEventV2) {
